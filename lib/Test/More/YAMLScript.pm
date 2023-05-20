409a7712921fe52dd8e0d39d666e96ccc1600ce0
Ingy döt Net <ingy@ingy.net>
2023-05-19 17:41:48 -0700

    Convert entire module to YAMLScript

--- lib/Test/More/YAMLScript.pm
+++ lib/Test/More/YAMLScript.pm
@@ -1,19 +1,4 @@
 use strict; use warnings;
 package Test::More::YAMLScript;
-
-our $VERSION = '0.1.0';
-
 use base 'Lingy::Namespace';
-
-no warnings 'redefine';
-
-sub subtest {
-    my ($t, $label, $exprs) = @_;
-    $t->subtest(
-        $label,
-        sub { Lingy::Eval::eval($_, $Lingy::Eval::ENV) for @_ },
-        @$exprs,
-    );
-}
-
-1;
+our $VERSION = '0.1.0';
